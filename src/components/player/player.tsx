07f2668062c9df903b56f11bc6d71427be543c57
razzh <razzhavenir@163.com>
2025-03-27 14:45:23 +0800

    docs: add type description

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -21,40 +21,53 @@ import '../../styles/main.scss';
  * @see https://aplayer.js.org/#/home?id=options
  */
 export interface APlayerProps {
+  /**
+   * @description audio info, should be an object or object array
+   */
   audio: AudioInfo | readonly AudioInfo[];
   /**
    * Initial volume
-   *
+   * @description default volume
    * @default 0.7
    */
   volume?: number;
 
   /**
+   * @description values: 'normal', 'fixed'
    * @default "normal"
    */
   appearance?: 'normal' | 'fixed';
 
   /**
+   * @description values: 'all' | 'one' | 'none'
    * @default "all"
    */
   initialLoop?: PlaylistLoop;
 
   /**
+   * @description values: 'list' | 'random'
    * @default "list"
    */
   initialOrder?: PlaylistOrder;
 
+  /**
+   * @description audio autoplay
+   * @default false
+   */
   autoPlay?: boolean;
 
   /**
+   * @description list max height
    * @default 250
    */
   listMaxHeight?: number;
   /**
+   * @description enable mini mode
    * @default false
    */
   mini?: boolean;
   /**
+   * @default prevent to play multiple player at the same time, pause other players when this player start play
    * @default true
    */
   mutex?: boolean;
